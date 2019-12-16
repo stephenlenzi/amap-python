c96dfdc9f1590f0707b41f32fefe730b89e3172e
Adam Tyson <adam.tyson@ucl.ac.uk>
2019-12-16 15:56:58 +0000

    warning about not newest version

--- amap/__init__.py
+++ amap/__init__.py
@@ -1,16 +1,16 @@
 __version__ = "0.0.10a3"
-
 __author__ = "Adam Tyson, Charly Rousseau, Christian Niedworok"
 __license__ = "GPL-3.0"
+__name__ = "amap"
 
 from . import *
 
 import luddite
 from packaging import version
 
-most_recent_version = luddite.get_version_pypi("amap")
+most_recent_version = luddite.get_version_pypi(__name__)
 
 if version.parse(__version__) < version.parse(most_recent_version):
-    print(f"This version of amap ({__version__}) is not the most recent "
+    print(f"This version of {__name__} ({__version__}) is not the most recent "
           f"version. Please update to v{most_recent_version} by running: "
-          f"'pip install amap --upgrade'")
+          f"'pip install {__name__} --upgrade'")
