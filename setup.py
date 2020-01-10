6f5f86c3fb59287fb0da0fc4b96dd8ae02cf5790
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-01-10 17:03:05 +0000

    bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [
 
 setup(
     name="amap",
-    version="0.1.7",
+    version="0.1.8",
     description="Automated mouse atlas propagation",
     install_requires=requirements,
     extras_require={
