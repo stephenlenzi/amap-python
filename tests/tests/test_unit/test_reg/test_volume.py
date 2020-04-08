4f60b77e4751b9dbad5f508a55b2387cfcc7d0fe
Stephen Lenzi <s.lenzi@ucl.ac.uk>
2020-04-08 16:56:05 +0100

    renames source custom config and import from imlib

--- tests/tests/test_unit/test_reg/test_volume.py
+++ tests/tests/test_unit/test_reg/test_volume.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from amap.register.volume import calculate_volumes
 from amap.config.atlas import Atlas
-from amap.tools.source_files import source_custom_config
+from amap.tools.source_files import source_custom_config_amap
 
 volume_data_path = os.path.join("tests", "data", "register", "volume")
 registered_atlas_path = os.path.join(volume_data_path, "registered_atlas.nii")
@@ -17,10 +17,10 @@ volumes_validate_path = os.path.join(volume_data_path, "volumes.csv")
 def test_volume_calc(tmpdir):
     tmpdir = str(tmpdir)
 
-    atlas = Atlas(source_custom_config())
+    atlas = Atlas(source_custom_config_amap())
 
     structures_file_path = atlas.get_structures_path()
-    registration_config = source_custom_config()
+    registration_config = source_custom_config_amap()
 
     volumes_csv_path = os.path.join(tmpdir, "volumes.csv")
     calculate_volumes(
